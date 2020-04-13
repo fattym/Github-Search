f8421bcccdcc78fb526e50d67ea0db9b9ac16160
fattym <jnganga288@gmail.com>
2020-04-13 13:13:21 +0300

    added repo functionality

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../services/profile.service';
+
 
 @Component({
   selector: 'app-repo',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo.component.css']
 })
 export class RepoComponent implements OnInit {
+  profiles: any;
+  repos: any;
+  username:string;
 
-  constructor() { }
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
+  }
+  findProfiles(){
+  	this.profileService.updateProfile(this.username);
+  	// tslint:disable-next-line: indent
+  	// tslint:disable-next-line: indent
+  	this.profileService.getProfileInfo().subscribe(profile => {
+  		console.log(profile);
+  		// tslint:disable-next-line: indent
+  		this.profiles = profile;
+  	// tslint:disable-next-line: indent
+  	});
+
+  	this.profileService.getProfileRepos().subscribe(repos => {
+  		console.log(repos);
+  		this.repos = repos;
+  	})
   }
 
 }
